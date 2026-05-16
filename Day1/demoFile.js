e155ea1760301168a00590b194f3e951038955c8
Your Name <your.email@example.com>
2026-05-16 20:41:09 +0700

    demo doc file TXT

---
+++ Day1/demoFile.js
@@ -0,0 +1,15 @@
+let fs = require('fs'); //fs = file system
+
+console.log('Start....');
+
+console.log('read file 1');
+let s1 = fs.readFileSync('data1.txt');
+
+console.log('result 1: ' + s1.toString()) //không dùng nháy ngược thì cộng chuỗi
+
+console.log('read file 2');
+let s2 = fs.readFileSync('data2.txt');
+
+console.log('result 1: ' + s2.toString())//do readFileSync trả về buffer nên phải dùng toString
+
+console.log('End. ')
