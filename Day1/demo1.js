428e5f88aa8caf45cd7fefc9694452e676b140bc
Your Name <your.email@example.com>
2026-05-16 19:34:24 +0700

    note về kí tự

--- Day1/demo1.js
+++ Day1/demo1.js
@@ -12,4 +12,4 @@ function add (a,b){
 c = add (a,b);
 // console.log(`the result of ${a} + ${b} = ${c}`);
 console.log(`the result of ${a} + ${b} = ${add(a,b)}`); //có thể gọi hàm trong ${} luôn
-
+//bắt đầu bằng nháy gì kết thúc bằng nháy đó, ko nhất thiết là nháy đôi - đơn hay ngược. nodejs ko có khái niệm kí tự- kể cả chuỗi
